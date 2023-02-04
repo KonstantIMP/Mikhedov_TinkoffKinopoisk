3112cc593d7469201db00c39c9b6bdf589d170e4
KonstantIMP <konstantimp@yandex.ru>
2023-02-04 11:53:35 +0300

    feat(app): add base ui for the films fragment

--- app/src/main/java/org/kimp/kinopoisk/tinkoff/ui/fragment/FilmsFragment.kt
+++ app/src/main/java/org/kimp/kinopoisk/tinkoff/ui/fragment/FilmsFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
+import org.kimp.kinopoisk.tinkoff.R
 import org.kimp.kinopoisk.tinkoff.databinding.FragmentFilmsBinding
 
 @AndroidEntryPoint
@@ -20,6 +21,13 @@ class FilmsFragment: Fragment() {
         binding = FragmentFilmsBinding.inflate(
             layoutInflater, container, false
         )
+
+        updateToolbarTitle()
+
         return binding.root
+    }
+
+    private fun updateToolbarTitle() {
+        binding.toolbar.title = getString(R.string.films_fragment_favorite_btn_text)
     }
 }
